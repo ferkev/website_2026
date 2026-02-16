475c2e3d877e4a4a6eed9bf22e552d0230435a16
Kevin LE FRANC <ferkev@live.fr>
2026-02-16 14:28:39 +0100

    add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,19 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+    imagesConfig: {
+      sizes: [320, 640, 1280],
+    },
+  }),
   output: "server",
   vite: {
     plugins: [tailwindcss()],
